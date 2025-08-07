b316a5d6101f388602699fc6565c974a0f8ba8c3
Charaf ZELLOU <charaf.zellou@gmail.com>
2025-08-08 00:02:18 +0200

    feat: enhance sidebar layout with separators and update title

--- frontend/app/components/Sidebar.vue
+++ frontend/app/components/Sidebar.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { separator } from '#build/ui';
+import { USeparator } from '#components';
+
 const links = [
   { label: 'Dashboard', to: '/game' },
   { label: 'Team', to: '/game/team' },
@@ -11,13 +14,19 @@ const links = [
 
 <template>
   <aside class="w-64 bg-gray-800 text-white p-4">
+    <USeparator />
     <h2 class="text-xl font-bold mb-8">
-      Main Menu
+      Football Pitch Tactics
     </h2>
+    <USeparator />
     <nav class="flex flex-col space-y-2">
       <UButton v-for="link in links" :key="link.to" :to="link.to" color="gray" variant="ghost" class="justify-start">
         {{ link.label }}
       </UButton>
     </nav>
+    <USeparator />
+    <p class="mt-16 text-sm text-gray-400">
+      © 2025 Football Pitch Tactics
+    </p>
   </aside>
 </template>
